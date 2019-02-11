9c427e10a2b1ff773572ac7513d1a63afdd93253
Takahiro Ueda <tueda@st.seikei.ac.jp>
2019-02-11 13:15:50 +0900

    Always run "make check" as in the "Installation" Wiki page

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,11 +6,16 @@
 #   travis.sh script   # Test the build
 #
 # Variables:
-#   VERSION (e.g., 4.2.0, HEAD) or REVISION (<commit-ish>) must be set
+#   IMAGE (e.g., "debian:stretch") is a docker image and must be set.
+#   VERSION (e.g., "4.2.0", "HEAD") or REVISION (<commit-ish>) must be set.
 
 set -eu
 set -o pipefail
 
+# Load "travis_retry".
+. ./scripts/bash/travis_retry.bash
+
+# TARGET = "release" or "devel" determined from $VERSION or $REVISION.
 if [ -n "${VERSION+x}" ]; then
   if [ -n "${REVISION+x}" ]; then
     echo "Error: both VERSION and REVISION given" >&2
@@ -28,6 +33,11 @@ else
   exit 1
 fi
 
+# Execute a command in the test container. This function can be called as
+#   docker_exec sudo <command...>
+#   docker_exec travis_retry sudo <command...>
+#   docker_exec <command...>
+#   docker_exec travis_retry <command...>
 docker_exec() {
   if [ $# -ge 1 ] && [ $1 = sudo ]; then
     shift
@@ -40,11 +50,11 @@ docker_exec() {
   fi
 }
 
-. ./scripts/bash/travis_retry.bash
-
 travis_install() {
+  # First, make a test container. The current directory is shared with the container.
   travis_retry docker run -d --name build_test -v "$(pwd):$(pwd)" "$IMAGE" tail -f /dev/null
 
+  # Fetch the FORM source.
   case $TARGET-$IMAGE in
     release-*)
       travis_retry wget https://github.com/vermaseren/form/releases/download/v$VERSION/form-$VERSION.tar.gz
@@ -60,54 +70,58 @@ travis_install() {
       ;;
   esac
 
+  # Install required packages.
   case $TARGET-$IMAGE in
-    release-debian:*|release-ubuntu:*)
-      docker_exec travis_retry sudo apt -q -y update
-      docker_exec travis_retry sudo apt -q -y install build-essential libgmp-dev zlib1g-dev
+    *-debian:*|*-ubuntu:*)
+      docker_exec travis_retry sudo apt-get -q -y update
+      docker_exec travis_retry sudo apt-get -q -y install build-essential libgmp-dev ruby zlib1g-dev
       ;;
+    *-fedora:*)
+      docker_exec travis_retry sudo dnf -q -y install gcc-c++ gmp-devel make ruby rubygem-test-unit zlib-devel
+      ;;
+    *-centos:*)
+      docker_exec travis_retry sudo yum -q -y install gcc-c++ gmp-devel make ruby rubygem-minitest zlib-devel
+      ;;
+  esac
+  case $TARGET-$IMAGE in
     devel-debian:*|devel-ubuntu:*)
-      docker_exec travis_retry sudo apt -q -y update
-      docker_exec travis_retry sudo apt -q -y install build-essential libgmp-dev zlib1g-dev
-      docker_exec travis_retry sudo apt -q -y install automake git ruby
-      ;;
-    release-fedora:*)
-      docker_exec travis_retry sudo dnf -q -y install gcc-c++ gmp-devel make zlib-devel
+      docker_exec travis_retry sudo apt-get -q -y install automake git
       ;;
     devel-fedora:*)
-      docker_exec travis_retry sudo dnf -q -y install gcc-c++ gmp-devel make zlib-devel
-      docker_exec travis_retry sudo dnf -q -y install automake git ruby rubygem-test-unit
-      ;;
-    release-centos:*)
-      docker_exec travis_retry sudo yum -q -y install gcc-c++ gmp-devel make zlib-devel
+      docker_exec travis_retry sudo dnf -q -y install automake git
       ;;
     devel-centos:*)
-      docker_exec travis_retry sudo yum -q -y install gcc-c++ gmp-devel make zlib-devel
-      docker_exec travis_retry sudo yum -q -y install automake git ruby rubygem-minitest
+      docker_exec travis_retry sudo yum -q -y install automake git
       ;;
   esac
 }
 
 travis_env() {
+  # Print information of the test container.
   docker_exec $(pwd)/scripts/print_env.sh
 }
 
 travis_script() {
+  # Run the test.
   case $TARGET-$IMAGE in
-    release-*)
-      docker_exec ./configure
-      docker_exec make
-      docker_exec sudo make install
-      ;;
     devel-*)
       docker_exec autoreconf -i
+      ;;
+  esac
+  case $TARGET-$IMAGE in
+    *)
       docker_exec ./configure
       docker_exec make
       docker_exec make check
       docker_exec sudo make install
+      docker_exec ls -l /usr/local/bin/form
+      docker_exec ls -l /usr/local/bin/tform
+      docker_exec ls -l /usr/local/share/man/man1/form.1
       ;;
   esac
 }
 
+# The entry point.
 case "$1" in
   install|env|script)
     travis_$1
